97f44ecddc300b873b6aa9e04533e31c2e7d1fcb
KerrickShiu <kshiu8@my.centennialcollege.ca>
2025-09-18 11:03:48 -0400

    added logo

--- client/components/Layout.jsx
+++ client/components/Layout.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from './logo.png';
+
 export default function Layout() {
 return (
 <>
 <h1>My Portfolio</h1>
+<img src={logo} length ='125em'width='125em'></img>
 <nav>
 <Link to="/">Home</Link> | <Link to="/about">About</Link> | <Link
 to="/education">Education</Link>| <Link to="/project">Project</Link>| <Link
