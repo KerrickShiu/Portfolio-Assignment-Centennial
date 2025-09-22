163db76894748f66e97fd30162981b8cef16ae3c
KerrickShiu <kshiu8@my.centennialcollege.ca>
2025-09-22 16:15:07 -0400

    Completed the Education Page

--- client/src/education.jsx
+++ client/src/education.jsx
@@ -1,7 +1,8 @@
 export default function Education() {
 return (
 <div>
-<p>Education.Qualification</p>
+<li>Sir John A. Macdonald CI OSSD 2022</li>
+<li>Centennial College Health Informatics Technology Advanced Diploma 2025-Present</li>
 </div>
 );
 }
